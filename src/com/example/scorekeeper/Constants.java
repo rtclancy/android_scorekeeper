3edbcf2b191d3f71d946f32dc35645e882b1c547
rtclancy <rtclancy@comcast.net>
2015-05-14 13:13:21 -0400

    Delete Constants.java

--- src/com/example/scorekeeper/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.scorekeeper;
-
-public interface Constants {
-	final int UPDATE_PLAYERS_RC = 1;
-	final int SELECT_COURSE_RC = 2;
-	final int VIEW_SCORECARD_RC = 3;
-	final int UPDATE_SCORE_RC = 4;
-}
